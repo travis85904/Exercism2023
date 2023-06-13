b8c3a6fa3a5a955e97ed45260bbca7a099a96e05
Baskeldo <basky981@gmail.com>
2023-06-13 13:53:35 -0600

    initial

---
+++ java/acronym/src/main/java/Acronym.java
@@ -0,0 +1,17 @@
+final class Acronym {
+    StringBuilder sb = new StringBuilder();
+
+    Acronym(String phrase) {
+        phrase = phrase.toUpperCase().replaceAll("[^A-Z- ]", "");
+        String[] words = phrase.split("[ -]");
+        for (String s : words) {
+            if (s.length() > 0)
+                sb.append(s.charAt(0));
+        }
+    }
+
+    String get() {
+        return sb.toString();
+    }
+
+}
